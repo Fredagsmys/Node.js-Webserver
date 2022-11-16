29b38a3c4c4fc1640c54b4be4e21e8fce4da8c25
Max <max.matttsson00@gmail.com>
2022-11-16 23:54:12 +0100

    small changes

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'http://magge321.ddns.net/', // Replace with your host name
+  host: 'http://magge321.ddns.net', // Replace with your host name
   user: 'fredagsmys',      // Replace with your database username
   password: '123',      // Replace with your database password
-  database: 'turbo_bathroom_db' // // Replace with your database Name
+  database: 'arduinodb' // // Replace with your database Name
 }); 
 conn.connect(function(err) {
   if (err) throw err;
