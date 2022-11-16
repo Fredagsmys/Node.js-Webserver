f1c82b355587f97e490e119962a10de3551a6ebf
Max <max.matttsson00@gmail.com>
2022-11-16 23:40:12 +0100

    small

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host: 'localhost', // Replace with your host name
+  host: 'localhost:3306', // Replace with your host name
   user: 'fredagsmys',      // Replace with your database username
   password: '123',      // Replace with your database password
   database: 'turbo_bathroom_db' // // Replace with your database Name
